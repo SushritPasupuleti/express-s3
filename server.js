4c7425d28ed74810a3a701325177717878f66f5b
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-03-28 16:50:35 +0530

    add keys from .env

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const multerS3 = require('multer-s3');
 const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3({
-    accessKeyId: 'access-key-id',
-    secretAccessKey: 'secret-access-key'
+    accessKeyId: process.env.accessKeyId,
+    secretAccessKey: process.env.secretAccessKey,
 });
