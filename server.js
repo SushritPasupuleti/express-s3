d4a8c037e0370402d49b8d8ed5f8d404cb192d6e
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-03-28 17:02:10 +0530

    fix issue with dotenv

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
-const multer  = require('multer');
+const multer = require('multer');
 const multerS3 = require('multer-s3');
 const AWS = require('aws-sdk');
 
@@ -8,3 +10,23 @@ const s3 = new AWS.S3({
     accessKeyId: process.env.accessKeyId,
     secretAccessKey: process.env.secretAccessKey,
 });
+
+const uploadS3 = multer({
+    storage: multerS3({
+        s3: s3,
+        acl: 'public-read',
+        bucket: process.env.AWSBucketName,
+        metadata: (req, file, cb) => {
+            cb(null, { fieldName: file.fieldname })
+        },
+        key: (req, file, cb) => {
+            cb(null, Date.now().toString() + '-' + file.originalname)
+        }
+    })
+});
+
+app.post('/upload', uploadS3.single('file'), (req, res) => {
+    console.log(req.file);
+});
+
+app.listen(process.env.PORT || 3000);
