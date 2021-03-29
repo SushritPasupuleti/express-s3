3ea148d800fc77ecca8f9c46ce8281dd62b304d4
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-03-29 14:01:27 +0530

    set up cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
+const cors = require('cors');
 var express = require('express'),
     aws = require('aws-sdk'),
     bodyParser = require('body-parser'),
@@ -16,6 +17,15 @@ var app = express(),
     s3 = new aws.S3();
 
 app.use(bodyParser.json());
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", "true");
+    //res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 var upload = multer({
     storage: multerS3({
